6f6c4b1cd8dfb39bdda11e52b6c3c01e9dc5537b
EmilianoAprile <emilianoaprile77@icloud.com>
2024-05-23 15:01:41 +0200

    add > fetch Data call

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+
+const fetchData = () => {
+    fetch(`https://api.chucknorris.io/jokes/random`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+};
+fetchData()
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
